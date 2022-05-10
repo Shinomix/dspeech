8a3e919d27b854d26a35aa9846236a35ea1b8154
Maxence Haltel <maxence.haltel@shopify.com>
2022-05-10 11:46:39 +0200

    Allow unlimited gas fees with local hardhat server

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,9 @@ module.exports = {
   solidity: "0.8.4",
   defaultNetwork: "rinkeby",
   networks: {
-     hardhat: {},
+     hardhat: {
+      allowUnlimitedContractSize: true
+     },
      rinkeby: {
         url: API_URL,
         accounts: [`0x${PRIVATE_KEY}`]
